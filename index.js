73ad3483a8317442375f89b5a379c61fa066cba6
Yeminfu <yeminfu4319@gmail.com>
2023-07-21 20:29:21 +1000

    create links getter

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 import puppeteer from 'puppeteer';
-console.log('go');
-let defaultLinks = true && [
-    'https://xn--27-vlcpka1acz.xn--p1ai/product-category/тест-драйв-и-аренда-техники/',
-    'https://xn--27-vlcpka1acz.xn--p1ai/product-category/велосипеды/',
-    'https://xn--27-vlcpka1acz.xn--p1ai/product-category/прицепы-для-водной-и-мототехники/',
-    'https://xn--27-vlcpka1acz.xn--p1ai/product-category/запчасти-и-аксессуары/',
-    'https://xn--27-vlcpka1acz.xn--p1ai/product-category/спасибо/',
-    'https://xn--27-vlcpka1acz.xn--p1ai/product-category/о-нас/'
-];
+import getLinksFromSidebar from './tools/getLinksFromSidebar.js';
+import defaultLinksa  from './db/defaultLinks.json.js';
+let defaultLinks = true && defaultLinksa;
+console.log('gonax!');
 
 (async () => {
     const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
     if (defaultLinks) {
-        // return defaultLinks;
+        console.log(defaultLinks);
+        return defaultLinks;
     }
     await page.goto('https://мотохит27.рф');
     const links = await getLinksFromSidebar(page);
@@ -24,13 +20,3 @@ let defaultLinks = true && [
     await browser.close();
 })();
 
-async function getLinksFromSidebar(page) {
-    return await page.evaluate(function () {
-        const linksFromNavbarNodes = document.querySelectorAll('.position-sticky a')
-        const linksFromNavbarNodesArray = Array.from(linksFromNavbarNodes);
-        const linksFromNavbarNodesArrayHrefs = linksFromNavbarNodesArray.map(({ href }) => href);
-        const linksFromNavbarNodesArrayHrefsCategories = linksFromNavbarNodesArrayHrefs.filter(link => /\/product-category\//.test(link))
-        const linksFromNavbarNodesArrayHrefsCategoriesDecoded = linksFromNavbarNodesArrayHrefsCategories.map(link => decodeURI(link));
-        return linksFromNavbarNodesArrayHrefsCategoriesDecoded;
-    });
-}
