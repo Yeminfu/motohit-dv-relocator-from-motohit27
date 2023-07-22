2f246a227464d4382e594fb4e1f52bf9b981cdec
Yeminfu <yeminfu4319@gmail.com>
2023-07-22 11:13:43 +1000

    default links switcher

--- index.js
+++ index.js
@@ -2,30 +2,35 @@ import puppeteer from 'puppeteer';
 import getLinksFromSidebar from './tools/getLinksFromSidebar.js';
 import getDataFromCategoryPage from './tools/getDataFromCategoryPage.js';
 import cleardbFolder from './tools/cleardbFolder.js';
+import defaultLinks from './defaultLinks.json.js';
 
-// import defaultLinksa from './db/defaultLinks.json.js';
-// let defaultLinks = true && defaultLinksa;
+let defaultLinksIsOn = true;
 console.log('gonax!');
-
-
 
 (async () => {
 
-    cleardbFolder()
+    cleardbFolder();
 
     const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
 
-    await page.goto('https://мотохит27.рф');
-    const links = await getLinksFromSidebar(page);
+    let links;
+
+    if (defaultLinksIsOn) {
+        console.log('нахуй главную');
+        links = defaultLinks;
+    } else {
+        console.log('открываем главную');
+        await page.goto('https://мотохит27.рф');
+        links = await getLinksFromSidebar(page);
+    }
 
     for (let index = 0; index < links.length; index++) {
         const link = links[index];
         console.log(link);
         const products = await getDataFromCategoryPage(page, link);
-        console.log(products);
+        console.log('products', products);
     }
 
     await browser.close();
 })();
-
