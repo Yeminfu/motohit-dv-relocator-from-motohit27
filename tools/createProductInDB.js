7264d73bdd74bc4aec6d4857f9d7ff5d209c5f93
Yeminfu <yeminfu4319@gmail.com>
2023-07-26 16:32:29 +1000

    rm bad quotes

--- tools/createProductInDB.js
+++ tools/createProductInDB.js
@@ -31,8 +31,8 @@ export default async function createProductInDB(category_name, dataFromProductPa
     });
 
     const values = [
-        ["product_name", `"${JSON.stringify(dataFromProductPage.product_name)}"`],
-        ["slug", `"${slugify(transliterator(dataFromProductPage.product_name.replace(/[^ a-zA-Zа-яА-Я0-9-]/igm, "")))}"`],
+        ["product_name", `${JSON.stringify(dataFromProductPage.product_name)}`],
+        ["slug", `${slugify(transliterator(dataFromProductPage.product_name.replace(/[^ a-zA-Zа-яА-Я0-9-]/igm, "")))}`],
         ["description", `"${description}"`],
         ["price", dataFromProductPage.price ? dataFromProductPage.price : 0],
         ["category", categoryId],
