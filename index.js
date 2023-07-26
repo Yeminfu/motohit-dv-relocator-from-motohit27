72fecd541c3e15554c7046b1e58df725691d1304
Yeminfu <yeminfu4319@gmail.com>
2023-07-26 10:26:23 +1000

    create module

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ import getDataFromCategoryPage from './tools/getDataFromCategoryPage.js';
 // import cleardbFolder from './tools/cleardbFolder.js';
 import defaultLinks from './db/defaultLinks.json.js';
 import getDataFromProductPage from './tools/getDataFromProductPage.js';
-import transliterator from './tools/transliterator.js';
-import slugify from 'slugify';
-import db_connection from './tools/dbConnect.js';
+
+import createProductInDB from './tools/createProductInDB.js';
 
 let defaultLinksIsOn = false;
 console.log('go go go!');
@@ -62,37 +61,4 @@ console.log('go go go!');
 })();
 
 
-async function createProductInDB(category_name, dataFromProductPage) {
 
-    const categoryId = await (async () => {
-        const category = await new Promise(r => db_connection.query(`SELECT * FROM categories WHERE category_name="${category_name}"`, function (err, data) {
-            if (!data) r(null);
-            if (!data[0]) r(null)
-            r(data[0].id);
-        }));
-        return category;
-    })();
-
-    const values = [
-        ["product_name", `"${dataFromProductPage.product_name}"`],
-        ["slug", `"${slugify(transliterator(dataFromProductPage.product_name.replace(/[^ a-zA-Zа-яА-Я0-9-]/igm, "")))}"`],
-        ["description", `"${dataFromProductPage.description}"`],
-        ["price", dataFromProductPage.price],
-        ["category", categoryId],
-    ];
-
-    const qs = `INSERT INTO products ( ${values.map(x => x[0]).join(", ")} ) VALUES ( ${values.map(x => x[1]).join(", ")} )`;
-
-    console.log(qs);
-
-    const productId = await new Promise(r => db_connection.query(qs, function (err, data) {
-        if (err) {
-        }
-        console.log('err', err);
-        r(data.insertId);
-    }));
-
-    console.log('productId', productId);
-    return productId;
-
-}
---
+++ tools/createProductInDB.js
@@ -0,0 +1,38 @@
+import transliterator from './transliterator.js';
+import slugify from 'slugify';
+import db_connection from './tools/dbConnect.js';
+
+export default async function createProductInDB(category_name, dataFromProductPage) {
+
+    const categoryId = await (async () => {
+        const category = await new Promise(r => db_connection.query(`SELECT * FROM categories WHERE category_name="${category_name}"`, function (err, data) {
+            if (!data) r(null);
+            if (!data[0]) r(null)
+            r(data[0].id);
+        }));
+        return category;
+    })();
+
+    const values = [
+        ["product_name", `"${dataFromProductPage.product_name}"`],
+        ["slug", `"${slugify(transliterator(dataFromProductPage.product_name.replace(/[^ a-zA-Zа-яА-Я0-9-]/igm, "")))}"`],
+        ["description", `"${dataFromProductPage.description}"`],
+        ["price", dataFromProductPage.price],
+        ["category", categoryId],
+    ];
+
+    const qs = `INSERT INTO products ( ${values.map(x => x[0]).join(", ")} ) VALUES ( ${values.map(x => x[1]).join(", ")} )`;
+
+    console.log(qs);
+
+    const productId = await new Promise(r => db_connection.query(qs, function (err, data) {
+        if (err) {
+        }
+        console.log('err', err);
+        r(data.insertId);
+    }));
+
+    console.log('productId', productId);
+    return productId;
+
+}
