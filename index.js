49d27320461115e8146bf53e2bacc0306bf6ef7c
Yeminfu <yeminfu4319@gmail.com>
2023-07-28 06:14:31 +1000

    create imageworker module

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ import getDataFromProductPage from './tools/getDataFromProductPage.js';
 import createProductInDB from './tools/createProductInDB.js';
 import db_connection from './tools/dbConnect.js';
 
-import fs from "fs";
-import request from "request";
+import imageWorker from './tools/imageWorker.js';
 
 
 let defaultLinksIsOn = false;
@@ -20,38 +19,6 @@ console.log('go go go!');
 
     const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox'], });
     const page = await browser.newPage();
-
-    await (async () => {
-        const category_name = "Электросамокаты";
-        const link = "https://мотохит27.рф/product/моторная-лодка-пвх-roger-boat-4000-киль-большой";
-        const {
-            youtubeLink,
-            attributes,
-            imagesLinks,
-            ...dataFromProductPage
-        } = await getDataFromProductPage(link, page);
-
-        const productId = await createProductInDB(category_name, dataFromProductPage, page)
-
-        if (imagesLinks?.length) {
-            for (let index = 0; index < imagesLinks.length; index++) {
-                const link = imagesLinks[index];
-                await imageWorker(link, productId);
-
-            }
-        }
-
-        console.log({
-            productId,
-            youtubeLink,
-            attributes,
-            // imagesLinks
-        });
-        await browser.close();
-        await db_connection.close();
-    })();
-
-    return;
 
 
     let links;
@@ -74,63 +41,43 @@ console.log('go go go!');
         // console.log({ products, asd });
         if (!products) continue;
         productsLinks.push(...products);
+        // break;
     }
 
     for (let index = 0; index < productsLinks.length; index++) {
         const { link, product_name, category_name } = productsLinks[index];
         // console.log({ link, product_name, category_name });
         console.log(`product ${index} from ${productsLinks.length - 1}`);
-        const dataFromProductPage = await getDataFromProductPage(link, page);
+        // const dataFromProductPage = await getDataFromProductPage(link, page);
 
+        const {
+            youtubeLink,
+            attributes,
+            imagesLinks,
+            ...dataFromProductPage
+        } = await getDataFromProductPage(link, page);
 
-        createProductInDB(category_name, dataFromProductPage, page)
+        // createProductInDB(category_name, dataFromProductPage, page)
+        const productId = await createProductInDB(category_name, dataFromProductPage, page)
 
+        if (imagesLinks?.length) {
+            for (let index = 0; index < imagesLinks.length; index++) {
+                const link = imagesLinks[index];
+                await imageWorker(link, productId);
+            }
+        }
+
+        console.log({
+            productId,
+            youtubeLink,
+            attributes,
+            // imagesLinks
+        });
+        // break;
         // console.log('dataFromProductPage', dataFromProductPage);
     }
 
     await browser.close();
-    await db_connection();
+    await db_connection.close();
 
 })();
-
-
-
-async function imageWorker(link, productId) {
-    const imageName = link.split("/").pop();
-
-    const imageDownloaded = await imageLoader(link, imageName);
-    if (imageDownloaded) {
-        await new Promise(r => {
-            db_connection.query(
-                `INSERT INTO media (type, name, essense_id) VALUES ("product_image", "${imageName}", ${productId} )`,
-                function (err) {
-                    if (err) {
-                        console.log('insert Image To DB error', err);
-                    } else {
-                        console.log('image saved');
-                    }
-                    r();
-                }
-            )
-        });
-    } else {
-        console.log('no image');
-    }
-}
-
-async function imageLoader(uri, filename) {
-    return await new Promise(
-        r =>
-            request.head(uri, function (err, res, body) {
-                if (res.headers['content-length']) {
-                    const filePath = `${process.env.IMAGES_FOLDER}/${filename};`
-                    request(uri).pipe(fs.createWriteStream(filePath)).on('close', () => null);
-                    r(true);
-                } else {
-                    r(false);
-                    console.log('no image');
-                }
-
-            })
-    )
-};
---
+++ tools/imageWorker.js
@@ -0,0 +1,43 @@
+import request from "request";
+import fs from "fs";
+import db_connection from "./dbConnect.js";
+
+export default async function imageWorker(link, productId) {
+    const imageName = link.split("/").pop();
+
+    const imageDownloaded = await imageLoader(link, imageName);
+    if (imageDownloaded) {
+        await new Promise(r => {
+            db_connection.query(
+                `INSERT INTO media (type, name, essense_id) VALUES ("product_image", "${imageName}", ${productId} )`,
+                function (err) {
+                    if (err) {
+                        console.log('insert Image To DB error', err);
+                    } else {
+                        console.log('image saved');
+                    }
+                    r();
+                }
+            )
+        });
+    } else {
+        console.log('no image');
+    }
+}
+
+async function imageLoader(uri, filename) {
+    return await new Promise(
+        r =>
+            request.head(uri, function (err, res, body) {
+                if (res.headers['content-length']) {
+                    const filePath = `${process.env.IMAGES_FOLDER}/${filename};`
+                    request(uri).pipe(fs.createWriteStream(filePath)).on('close', () => null);
+                    r(true);
+                } else {
+                    r(false);
+                    console.log('no image');
+                }
+
+            })
+    )
+};
