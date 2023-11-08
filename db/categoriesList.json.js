2206e38794d8fbeccaef00a9f10fd69eb814b3d6
Yeminfu <yeminfu4319@gmail.com>
2023-11-08 21:00:57 +1000

    up

---
+++ db/categoriesList.json.js
@@ -0,0 +1,111 @@
+export default [
+    {
+        "category_name": "Электро транспорт",
+        "children": [
+            {
+                "category_name": "Электросамокаты"
+            },
+            {
+                "category_name": "Электровелосипеды"
+            },
+            {
+                "category_name": "Комплекты для электровелосипедов"
+            }
+        ]
+    },
+    {
+        "category_name": "Мотоциклы и мопеды"
+    },
+    {
+        "category_name": "Квадроциклы"
+    },
+    {
+        "category_name": "Снегоходы, Буксировщики, Болотоходы",
+        "children": [
+            {
+                "category_name": "Мотобуксировщики"
+            },
+            {
+                "category_name": "Снегоходы, миниснегоходы"
+            },
+            {
+                "category_name": "Вездеходы"
+            },
+            {
+                "category_name": "Сани-волокуши для мотобуксировщика/снегохода"
+            }
+        ]
+    },
+    {
+        "category_name": "Лодки",
+        "children": [
+            {
+                "category_name": "Лодки ПВХ"
+            },
+            {
+                "category_name": "Лодки ПНД"
+            },
+            {
+                "category_name": "Лодки аллюминиевые"
+            }
+        ]
+    },
+    {
+        "category_name": "Подвесные Лодочные моторы",
+        "children": [
+            {
+                "category_name": "Лодочные моторы"
+            },
+            {
+                "category_name": "Водометы и водометные насадки"
+            }
+        ]
+    },
+    {
+        "category_name": "Бензо электро инструмент(дом, дача)",
+        "children": [
+            {
+                "category_name": "Навесное к мотоблокам"
+            },
+            {
+                "category_name": "Мотоблоки (культиваторы)"
+            },
+            {
+                "category_name": "Двигатели внутреннего сгорания (ДВС)"
+            },
+            {
+                "category_name": "Пилы"
+            },
+            {
+                "category_name": "Тримеры"
+            },
+            {
+                "category_name": "Буры"
+            },
+            {
+                "category_name": "Генераторы"
+            },
+            {
+                "category_name": "Зарядки и пускозарядки"
+            },
+            {
+                "category_name": "Насосы"
+            },
+            {
+                "category_name": "Снегоотбрасыватели"
+            },
+            {
+                "category_name": "Дуйки и садовые пылесосы"
+            },
+            {
+                "category_name": "Мойки высокого давления"
+            }
+        ]
+    },
+    {
+        "category_name": "Велосипеды"
+    },
+    {
+        "category_name": "Прицепы для водной и мототехники"
+    }
+];
--- tools/clearProducts.js
+++ tools/clearProducts.js
@@ -1,72 +1,19 @@
 import fs from "fs";
 import path from "path";
-import db_connection from "./dbConnect.js";
-
+import db_connection, { pool } from "./dbConnect.js";
+import categoriesList from "../db/categoriesList.json.js";
+import slugify from "slugify";
+import transliterator from "./transliterator.js";
 
 export default async function clearProducts() {
-    await new Promise(r => {
-        db_connection.query(
-            `DELETE FROM attr_prod_relation`,
-            function (err, res) {
-                if (err) {
-                    console.log('Err #c83b2m', err);
-                }
-                r();
-            }
-            )
-    });
+    await db_connection.promise().query(`DELETE FROM attr_prod_relation`);
+    await db_connection.promise().query(`DELETE FROM attributes_values`);
+    await db_connection.promise().query(`DELETE FROM attributes`);
+    await db_connection.promise().query(`DELETE FROM media`);
+    await db_connection.promise().query(`DELETE FROM products`);
+    await db_connection.promise().query(`DELETE FROM categories WHERE parent IS NOT NULL`);
+    await db_connection.promise().query(`DELETE FROM categories`);
 
-    await new Promise(r => {
-        db_connection.query(
-            `DELETE FROM attributes_values`,
-            function (err, res) {
-                if (err) {
-                    console.log('Err #c83bsd2m', err);
-                }
-                r();
-            }
-            )
-    });
-
-    await new Promise(r => {
-        db_connection.query(
-            `DELETE FROM attributes`,
-            function (err, res) {
-                if (err) {
-                    console.log('Err #c3c2dcc', err);
-                }
-                r();
-            }
-        )
-    });
-
-
-
-    await new Promise(r => {
-        db_connection.query(
-            `DELETE FROM media`,
-            function (err, res) {
-                if (err) {
-                    console.log('Err #c3c2dcc', err);
-                }
-                r();
-            }
-        )
-    });
-
-
-    await new Promise(r => {
-        db_connection.query(
-            `DELETE FROM products`,
-            function (err, res) {
-                if (err) {
-                    console.log('Err #z02lf', err);
-                }
-                r();
-            }
-        )
-    });
-    
     const directory = process.env.IMAGES_FOLDER;
 
     fs.readdir(directory, (err, files) => {
@@ -78,6 +25,45 @@ export default async function clearProducts() {
             });
         }
     });
-    
 
+    for (let index = 0; index < categoriesList.length; index++) {
+        const category = categoriesList[index];
+        await categoryWorker(category);
+    }
+}
+
+async function categoryWorker(cat, parent_name) {
+    const { category_name, children } = cat;
+    if (!children) {
+        if (parent_name) { // создаем потомка-категорию
+            saveCategory(category_name, parent_name)
+        } else { // создаем глав категорию без детей
+            saveCategory(category_name);
+        }
+    } else { // создаем глав категорию с детьми
+        saveCategory(category_name);
+        for (let index = 0; index < children.length; index++) {
+            const child = children[index];
+            await categoryWorker(child, category_name);
+        }
+    }
+}
+
+async function saveCategory(category_name, parent) {
+    const categorySlug = slugify(transliterator(category_name));
+    const parentId = parent ? await getCategoryIdByName(parent) : null;
+    return await db_connection.promise().query(
+        "INSERT INTO categories (category_name,slug, parent) VALUES (?,?,?)",
+        [category_name, categorySlug, null]
+    )
+}
+
+async function getCategoryIdByName(name) {
+    return db_connection.promise().query(
+        "SELECT * FROM categories WHERE category_name = ?",
+        [name]
+    )
+        .then(([res]) => {
+            return res.shift().id;
+        });
 }
