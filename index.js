3ae1f2732f67c6fbdb062298c3ec7227e3f0f0f6
Yeminfu <yeminfu4319@gmail.com>
2023-11-11 15:56:53 +1000

    up

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import db_connection, { pool } from './tools/dbConnect.js';
 import imageWorker from './tools/imageWorker.js';
 import attributesWorker from './tools/attributesWorker.js';
 import clearProducts from './tools/clearProducts.js';
+import fs from "fs"
 
 console.log('go go go!');
 
@@ -18,9 +19,15 @@ console.log('go go go!');
     // const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox'], });
     // const page = await browser.newPage();
 
-    const products = await fetch("http://мотохит27.рф/wp-json/v1/products")
-        .then(x => x.json());
 
+    const products = JSON.parse(fs.readFileSync("./db/allProducts.json", "utf-8", () => null));
+    // console.log('products', products[0]);
+    // const products = await fetch("http://мотохит27.рф/wp-json/v1/products")
+    //     .then(x => x.json());
+
+    // fs.writeFile("./db/allProducts.json", JSON.stringify(products), () => null);
+
+    // return;
     const stockStatuses = {
         "instock": "в наличии в магазине",
         "isenroute": "в пути наличии на складе",
